872089eeb39ae1753d43d358edf203a70703d613
mariegrim <79672457+mariegrim@users.noreply.github.com>
2021-03-12 12:49:19 +0000

    Deploying to gh-pages from master @ 42b6334cb8204ff8f1c9e5e789af4a6433e722c3 🚀

---
+++ pipo_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="pipo_tiles" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
+ <image source="../../../Workadventure/pipo_[Base]BaseChip_pipo.png" width="256" height="4256"/>
+</tileset>
